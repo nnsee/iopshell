8f3114f5c288f1b7b7d15aa60b50e062bd61df8a
Rasmus Moorats <me@neonsea.uk>
2018-06-29 17:50:57 +0300

    cmd: make MinArg and MaxArg functional

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -14,15 +14,24 @@ type Command struct {
     UsageText   string
     Description string
     Action      func([]string)
-    MinArg      int
-    MaxArg      int
+    MinArg      int // Includes command name. 0 for unlimited
+    MaxArg      int // Same as above
 }
 
 func (cmd *Command) Usage() {
-    fmt.Println("Usage: ", cmd.UsageText)
+    fmt.Println("Usage:", cmd.UsageText)
 }
 
 func (cmd *Command) Execute(line string) {
-    arg := strings.Split(line, " ")[1:]
-    cmd.Action(arg)
+    arg := strings.Split(line, " ")
+    switch {
+    case cmd.MaxArg != 0 && len(arg) > cmd.MaxArg:
+        fmt.Println("Too many arguments")
+        cmd.Usage()
+    case cmd.MinArg != 0 && len(arg) < cmd.MinArg:
+        fmt.Println("Too few arguments")
+        cmd.Usage()
+    default:
+        cmd.Action(arg[1:])
+    }
 }
